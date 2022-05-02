99c2530a62d098fb0a8d60f871a9a76e01dcc9c4
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-05-02 14:40:41 +0200

    using ejs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const PORT = process.env.PORT || 3000
 
 const app = express()
 const server = require('http').createServer(app)
@@ -15,4 +16,4 @@ app.get('/', (request, response) => {
 
 })
 
-app.listen(3000, () => console.log(`Server start on port: 3000`))
+app.listen(PORT, () => console.log(`Server start on port: ${PORT}`))
